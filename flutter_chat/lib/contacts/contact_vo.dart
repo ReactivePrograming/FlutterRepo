22d4befdfb8f399c2677a5a9c48fbc31d8789049
Havi <Havi@192.168.124.2>
2019-10-24 20:51:37 +0800

    new

--- flutter_chat/lib/contacts/contact_vo.dart
+++ flutter_chat/lib/contacts/contact_vo.dart
@@ -13,8 +13,126 @@ class ContactVO {
   ContactVO({@required this.seationKey, this.name, this.avatarUrl});
 }
 
-List<ContactVO> = [
+List<ContactVO> contactData = [
   new ContactVO(
-
+    seationKey: 'A',
+    name: '张三',
+    avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'F',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'E',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'D',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'B',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'B',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'B',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'C',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'C',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'A',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'J',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'J',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'J',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'K',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'K',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'K',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
+  ),
+  new ContactVO(
+      seationKey: 'Z',
+      name: '张三',
+      avatarUrl: 'https://cn.bing.com/th?id=OIP.TiKxade-C2zcWIh2z4g5mQHaLH&o=5&pid=1.7&rs=1'
   )
+
 ];
--- flutter_chat/lib/contacts/contacts.dart
+++ flutter_chat/lib/contacts/contacts.dart
@@ -1,7 +1,10 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/contacts/contact_header.dart';
+import 'package:flutter_chat/contacts/contact_item.dart';
 import 'package:flutter_chat/contacts/contact_sider.dart';
+import 'package:flutter_chat/contacts/contact_vo.dart';
 
 class Contacts extends StatefulWidget {
 
@@ -20,7 +23,34 @@ class _ContactsState extends State<Contacts> {
     // TODO: implement build
     return new Scaffold(
       body: ContactSiderList(
-        items: conta,
+        items: contactData,
+        headerBuilder: (BuildContext context, int index){
+          return Container(
+            child: ContactHeader(),
+          );
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.white,
+            alignment: Alignment.centerLeft,
+            child: ContactItem(item: contactData[index],),
+          );
+        },
+        sectionBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 32,
+            padding: const EdgeInsets.only(left: 14),
+            color: Colors.grey[300],
+            alignment: Alignment.centerLeft,
+            child: Text(
+              contactData[index].seationKey,
+              style: TextStyle(
+                fontSize: 14,
+                color: Color(0xff909090),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
