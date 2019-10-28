80cbd4c9829ef37c7d48f8eaf4edfccb79173115
Havi <1058695900@qq.com>
2019-10-28 16:58:07 +0800

    stock

---
+++ flutter_stock/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(StockApp());
+
+class StockApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class StockAppState extends State<StockApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
